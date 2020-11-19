0892d5d673c0076bd819416d28dcd301b0d3810d
mounaim <mounaim.zaryouhi@gmail.com>
2020-11-19 13:44:50 +0100

    some refactoring in covid_stats_json.py file

--- covid_stats_json.py
+++ covid_stats_json.py
@@ -11,37 +11,10 @@ def merge_two_dicts(x, y):
     z = x.copy()
     z.update(y)
     return z
-""" def get_coronavirus_json() : 
-    scraping_folder = path.join(path.dirname(__file__), 'CSVs')
-    print ("scraping folder : ", scraping_folder)
-    filename = utils.get_todays_fileName("csv")
-    filename_path = path.join(scraping_folder,filename)
-    print("complete csv path : ", filename_path)
-
-    
-
-    # read csv file and convert it to json
-    with open(filename_path,mode = 'r',encoding = 'utf-8') as file :
-        cities_data = {}
-        reader = csv.reader(file)
-        iteration = 0
-        for row in reader : 
-            #skip the header row
-            if iteration == 0 :
-                iteration+=1 
-                continue
-            #print(row)
-            cities_data [row[1]] = {'new_cases' : int(row[2]),'deaths' : int(row[3])}
-
-    # read national covid-19 data from read_stats.read_stats()
-    national_data = read_stats.read_stats()
-
-    # merge the two into one single json
-    final_data = merge_two_dicts(national_data,cities_data)
-    return final_data """
 
 """
-date is provided in dd-MM-yy with non padding zero when day or month are <10
+generates all coronavirus-2 data for some date
+date is provided in "dd-MM-yy" format with non padding zero when day or month are <10
 """
 def get_coronavirus_json(date) : 
     csv_filename = "corona_{}.csv".format(date)
